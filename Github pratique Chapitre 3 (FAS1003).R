45d8da16f137b5b33f6faa8f8a4ba61a8674d79c
Mackla44 <joackim.lyonnais@umontreal.ca>
2024-01-16 06:06:42 +0000

    Initial commit

---
+++ Github pratique Chapitre 3 (FAS1003).R
@@ -0,0 +1,20 @@
+library(usethis)
+library(gitcreds)
+
+create_github_token()
+gitcreds_set()
+
+git_vaccinate()
+
+git_sitrep()
+
+use_git_config()
+
+use_git_config(
+  user.name = "Mackla44",
+  user.email = "joackim.lyonnais@umontreal.ca"
+)
+
+use_git()
+
+use_github()
